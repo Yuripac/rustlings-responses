f782e679415c4af0f3d5f8df3ac55ab70a5612aa
yuripac <yuri_dias_@hotmail.com>
2023-04-04 00:41:44 -0300

    hashmap done

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,8 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -35,6 +33,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
 
     for fruit in fruit_kinds {
         // TODO: Put new fruits if not already present. Note that you
+        basket.entry(fruit).or_insert(1);
         // are not allowed to put any type of fruit that's already
         // present!
     }
